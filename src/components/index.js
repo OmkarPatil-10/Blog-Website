2b08541dd4066b462b73c8fa5f2293ebf92b45f9
OmkarPatil-10 <omkarpatil9935@gmail.com>
2025-07-01 00:48:33 +0530

    modified

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "../container/container";
+import Container from "../container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
 import Button from "./Button";
